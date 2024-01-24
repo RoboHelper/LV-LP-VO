ab97b0a7712db9c63f348e63f98104955d4bc38e
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-01-24 23:24:49 +0300

    V1

---
+++ scripts/g-tag.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "AW-11107979629");
+
+gtag("event", "conversion", {
+  send_to: "AW-11107979629/NQUxCOqnhoEZEO2i2bAp",
+  value: 1.0,
+  currency: "USD",
+});
