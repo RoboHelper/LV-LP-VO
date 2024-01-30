6355eb924a8e038a3dddcce73f17ebf256489e07
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-01-30 16:59:12 +0300

    HR7 settings applied

--- configs.js
+++ configs.js
@@ -1,17 +1,17 @@
-const domainName = "helpforallusa.com";
+const domainName = "americanledfreedom.com";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
+const G2DisplayName = "Cameron Webster Lenhoff- G2 Certification";
 const G2Link =
-  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
+  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2200";
 
 const addLeadId = true;
 
-const qualifyNumber = "+18339320043";
-const overflowNumber = "+18339390163";
+const qualifyNumber = "+18555460048";
+const overflowNumber = "+18555471404";
 
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "AW-11107979629";
-const googleConversionTagEventSentTo = "AW-11107979629/NQUxCOqnhoEZEO2i2bAp";
+const googleAccountConversionTagID = "AW-11476003059";
+const googleConversionTagEventSentTo = "AW-11476003059/NSgsCODPg40ZEPPRl-Aq";
 const googleConversionTagValue = 1.0;
