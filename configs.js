4a918de5f24826162e3a22eb18697c11c2490f79
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-01-30 20:39:28 +0300

    G Tag fixed

--- configs.js
+++ configs.js
@@ -1,17 +1,17 @@
-const domainName = "helpforallusa.com";
+const domainName = "allamericanassistance.com";
 
-const G2DisplayName = "Peak Plans LLC. - G2 Certification";
+const G2DisplayName = "Pivotal Concepts Inc. - G2 Certification";
 const G2Link =
-  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2314";
+  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2411";
 
 const addLeadId = true;
 
-const qualifyNumber = "+18339320043";
-const overflowNumber = "+18339390163";
+const qualifyNumber = "+18556754844";
+const overflowNumber = "+18557012637";
 
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "AW-11107979629";
-const googleConversionTagEventSentTo = "AW-11107979629/NQUxCOqnhoEZEO2i2bAp";
+const googleAccountConversionTagID = "AW-11483465906";
+const googleConversionTagEventSentTo = "AW-11483465906/Rcc_CJzloI0ZELKR3-Mq";
 const googleConversionTagValue = 1.0;
