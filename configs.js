579cf278050afe003f3548eef7bb670b1512ca1c
Qasim-Hayee <150183455+QasimHayeeK@users.noreply.github.com>
2024-02-05 20:46:56 +0500

    HU created

--- configs.js
+++ configs.js
@@ -1,17 +1,17 @@
 const domainName = "allamericanassistance.com";
 
-const G2DisplayName = "Pivotal Concepts Inc. - G2 Certification";
+const G2DisplayName = "Reclaim Insurance Agency";
 const G2Link =
-  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=2411";
+  "https://www.g2llc.com/approved-health-insurance-provider/?pdb=1237";
 
 const addLeadId = true;
 
-const qualifyNumber = "+18556754844";
-const overflowNumber = "+18557012637";
+const qualifyNumber = "+18338891275";
+const overflowNumber = "+18445942134";
 
 const overflowRingbaCampaignID = "CA7f1be56c7e6f4e229cb806b4c76add9a";
 const normalRingbaCampaignID = "CA0457ccda8864417991b332abbcc7951c";
 
-const googleAccountConversionTagID = "AW-11483465906";
-const googleConversionTagEventSentTo = "AW-11483465906/Rcc_CJzloI0ZELKR3-Mq";
-const googleConversionTagValue = 1.0;
+const googleAccountConversionTagID = "AW-11108069111";
+const googleConversionTagEventSentTo = "AW-11108069111/a-GaCNvSko8ZEPfd3rAp";
+const googleConversionTagValue = 15.0;
